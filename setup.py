83f380791eea92d01e47ba24cc16b21676b535cd
ziwazou <yanisziouani4@gmail.com>
2025-10-05 23:24:58 +0200

    install_requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot_RAG_system",
+    version="0.1.0",
+    author="Ziouani Yanis",
+    author_email="yanisziouani4@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
